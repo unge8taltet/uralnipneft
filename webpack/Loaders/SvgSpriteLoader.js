ce0bf87dc9382c962c87c775051bceed2aeed76d
unge8taltet <unge8taltet@gmail.com>
2022-07-04 19:43:01 +0300

    first commit

---
+++ webpack/Loaders/SvgSpriteLoader.js
@@ -0,0 +1,10 @@
+/**
+ * Загрузчик веб-пакетов для создания SVG-спрайтов.
+ *
+ * @return {Object} Config
+ * @see https://github.com/JetBrains/svg-sprite-loader
+ */
+
+module.exports = {
+    loader: 'svg-sprite-loader'
+}
